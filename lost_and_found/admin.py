d5088eb1b9c794cf7409b67603bf50a4b04df34d
Tanya Gautam <gautamt@uwindsor.ca>
2024-06-12 03:45:22 -0400

    updated admin.py-minor error

--- lost_and_found/admin.py
+++ lost_and_found/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from .models import FoundPet, LostPet
 
-# Register your models here.
 @admin.register(FoundPet)
+class FoundPetAdmin(admin.ModelAdmin):
+    list_display = ('species', 'location', 'date_posted', 'user')
+    search_fields = ('species', 'location')
+    list_filter = ('date_posted',)
+
 @admin.register(LostPet)
+class LostPetAdmin(admin.ModelAdmin):
+    list_display = ('species', 'location', 'date_posted', 'user', 'pet_name')
+    search_fields = ('species', 'location', 'pet_name')
+    list_filter = ('date_posted',)
