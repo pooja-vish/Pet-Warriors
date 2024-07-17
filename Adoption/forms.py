91631c0325f1e50a5c623b8b6e051aeb1cbd60ec
pooja-vish <vishwak@uwindsor.ca>
2024-07-17 13:44:52 -0400

    search bar and adoption form functionality

---
+++ Adoption/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Adoption
+
+
+class AdoptionForm(forms.ModelForm):
+    class Meta:
+        model = Adoption
+        fields = ['image', 'description', 'location', 'species', 'user_id']
+        widgets = {
+            'location': forms.TextInput(
+                attrs={'class': 'form-control border-0 p-4', 'id': 'location', 'name': "location",
+                       'placeholder': 'Enter Location', 'required': 'required'}),
+            'description': forms.TextInput(
+                attrs={'class': 'form-control border-0 p-4', 'id': 'description', 'name': "description",
+                       'placeholder': 'Enter description', 'required': 'required'}),
+            'species': forms.TextInput(
+                attrs={'class': 'form-control border-0 p-4', 'id': 'species', 'name': "species",
+                       'placeholder': 'Enter Species', 'required': 'required'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+        }
+
+class AdoptionSearchForm(forms.Form):
+    species = forms.CharField(required=False,label='',widget=forms.TextInput(attrs={'class': 'form-control form-control-lg', 'placeholder': 'Enter species here babu'}))
