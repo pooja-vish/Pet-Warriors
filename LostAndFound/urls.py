ac4c15f2f7c9ea7c5ceaa0325fa1447be1f34f4b
Tanya Gautam <149003784+GautamTanya13@users.noreply.github.com>
2024-08-05 21:33:12 -0400

    Update lost and found urls.py

--- LostAndFound/urls.py
+++ LostAndFound/urls.py
@@ -6,4 +6,6 @@ app_name = 'LostAndFound'
 urlpatterns = [
     path('lost/', views.lost, name='lost'),
     path('found/', views.found, name='found'),
+    path('lostSearch',views.lostSearch, name='lostSearch'),
+    path('foundSearch',views.foundSearch, name='foundSearch'),
 ]
